fe740d53fd8e9cefd9a8887a3b6952a0a74ccdeb
Oliver Balog <redhoodiegamer123@gmail.com>
2024-12-12 10:04:17 +0100

    Made GameObject class abstract

--- GameObject.cs
+++ GameObject.cs
@@ -1,13 +1,27 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Platformer
 {
-    public class GameObject
+    public abstract class GameObject
     {
+        protected Texture2D texture;
+        protected Rectangle size;
 
+        public Rectangle Size
+        {
+            get { return size; }
+        }
+        public GameObject(Rectangle rec)
+        {
+            this.size = rec;
+        }
+
+        public abstract void Draw(SpriteBatch spriteBatch);
     }
 }

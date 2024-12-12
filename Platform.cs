8e42354630428e6f9700a63575638ebebae9d66c
Oliver Balog <redhoodiegamer123@gmail.com>
2024-12-12 10:08:43 +0100

    GameObject heirarchy added

--- Platform.cs
+++ Platform.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Platformer
 {
-    public class Platform
+    public class Platform :GameObject
     {
 
     }
--- Player.cs
+++ Player.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Platformer
 {
-    public class Player
+    public class Player : GameObject
     {
 
     }

82fe55cdacc582f4fd9b6d4ef627c30bb4daad31
Oliver Balog <redhoodiegamer123@gmail.com>
2024-12-12 10:01:45 +0100

    AssetManager

--- AssetManager.cs
+++ AssetManager.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 
 namespace Platformer
 {
-    public class AssetManager
+    public static class AssetManager
     {
+        public static Texture2D playerSpriteSheet;
+        public static Texture2D background;
+        public static Texture2D platform;
+        public static Texture2D spriteSheet;
 
+        public static void LoadTextures(ContentManager content)
+        {
+            playerSpriteSheet = content.Load<Texture2D>("IndianaJones");
+            background = content.Load<Texture2D>("Ground");
+            platform = content.Load<Texture2D>("plattform");
+            spriteSheet = content.Load<Texture2D>("Sheet_BubbleBobble");
+        }
     }
 }
